c4d272e726728e0a86cfc1a2cd519bc51f6eac90
greyrainn <greyayoun@gmail.com>
2025-12-30 11:46:32 +0200

    fix typo and add script

---
+++ script.js
@@ -0,0 +1,6 @@
+const myText = "I am a string";
+const newString = myText.replace("string", "sausage");
+console.log(newString);
+// the replace() string function takes a source string,
+// and a target string and replaces the source string,
+// with the target string, and returns the newly formed string
