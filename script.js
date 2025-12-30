df6a38c5ead3a64b2cd779b6ed016611dad4357f
greyrainn <greyayoun@gmail.com>
2025-12-30 12:03:49 +0200

    add text box and css file

--- script.js
+++ script.js
@@ -4,3 +4,27 @@ console.log(newString);
 // the replace() string function takes a source string,
 // and a target string and replaces the source string,
 // with the target string, and returns the newly formed string
+const myArray = ["I", "love", "chocolate", "frogs"];
+const madeAString = myArray.join(" ");
+console.log(madeAString);
+// returns 'I love chocolate frogs'
+
+const madeAnotherString = myArray.join();
+console.log(madeAnotherString);
+// returns 'I,love,chocolate,frogs'
+
+const originals = [1, 2, 3];
+
+
+
+
+const doubled = originals.map(function (item) {
+  return item * 2;
+});
+console.log(doubled); // [2, 4, 6]
+const textBox = document.querySelector("#textBox");
+const output = document.querySelector("#output");
+
+textBox.addEventListener("keydown", (event) => {
+  output.textContent = `You pressed "${event.key}".`;
+});
